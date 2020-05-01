ba4599399dd9ba0c0f2983b06aab55efa6949a9a
Armino Popp <devel@boioiong.com>
2020-05-01 15:50:44 +0300

    Moved header and footer to their own components

---
+++ components/footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <div className="attribution">
+                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+                Coded by <a href="https://github.com/armino-dev" title="See Armino's profile on GitHub" target="_blank">Armino</a>.
+            </div>            
+        </footer>        
+    )
+}
---
+++ components/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Where in the world?</h1>
+            <a href="#" className="btn btnToggleTheme"><i className="fa fa-moon-o"></i> Dark Mode</a>
+        </header>
+    )
+}
