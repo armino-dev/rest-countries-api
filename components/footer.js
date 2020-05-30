1c0ed9984dc605598ca66d3c99631514272bce23
Armino Popp <devel@boioiong.com>
2020-05-30 10:42:15 +0300

    Minor fixes for footer and mobile padding

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,11 @@
-export default function Footer() {
-    return (
-        <footer>
-            <div className="attribution">
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
-                Coded by <a href="https://github.com/armino-dev" title="See Armino's profile on GitHub" target="_blank">Armino</a>.
-            </div>            
-        </footer>        
-    )
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <div className="attribution">
+                    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a href="https://github.com/armino-dev" title="See Armino's profile on GitHub" target="_blank">Armino</a>.
+                </div>            
+            </footer>        
+        )
+    }    
 }
